d38d14ad417fea6ae9bbbc22e95151663d4594c1
Brian <brtabarez@alumno.unlam.edu.ar>
2023-04-27 21:56:45 -0300

    Room implementation & login fragment design

--- app/src/main/java/com/example/lexiapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/lexiapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.lexiapp
+package com.example.lexiapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.lexiapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
