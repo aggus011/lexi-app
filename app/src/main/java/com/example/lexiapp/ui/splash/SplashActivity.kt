2de3022cfd30e3eb8ea92e5cb0f0cffd07ca1678
Lucas-Zacarias <lucas21111999@gmail.com>
2023-05-01 19:33:07 -0300

    splash screen design added

---
+++ app/src/main/java/com/example/lexiapp/ui/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lexiapp.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lexiapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
