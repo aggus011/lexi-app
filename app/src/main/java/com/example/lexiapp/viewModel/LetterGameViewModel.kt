cabb9e87e07850ebc3112d5edb0bb40548564a7f
Brian <brtabarez@alumno.unlam.edu.ar>
2023-05-10 16:36:34 -0300

    Implement ViewModel

--- app/src/main/java/com/example/lexiapp/viewModel/LetterGameViewModel.kt
+++ app/src/main/java/com/example/lexiapp/viewModel/LetterGameViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.lexiapp.viewModel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.lexiapp.domain.Repository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
@@ -8,25 +9,27 @@ import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-class LetterGameViewModel(repository: Repository): ViewModel() {
+class LetterGameViewModel(repository: Repository) : ViewModel() {
 
     private var _words = MutableStateFlow<List<String>>(listOf())
     val words = _words.asStateFlow()
     private var basicWord: String? = null
 
     init {
-        /*
         viewModelScope.launch(Dispatchers.IO) {
             repository.getWordToLetterGame()
                 .collect {
-
+                    basicWord = it
                 }
+            generateWordVariants()
         }
-
-         */
     }
 
+    private fun generateWordVariants() {
+        TODO()
+    }
 
 
 }
