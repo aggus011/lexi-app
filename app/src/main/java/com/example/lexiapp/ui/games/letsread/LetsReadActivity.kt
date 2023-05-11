6918e92f3624a4871468c4a7bff35b1148b25ab3
aitorLob0 <aolaechealobo@alumno.unlam.edu.ar>
2023-05-11 16:12:57 -0300

    init commit -layout activity -logic creation of button letters

--- app/src/main/java/com/example/lexiapp/ui/games/letsread/LetsReadActivity.kt
+++ app/src/main/java/com/example/lexiapp/ui/games/letsread/LetsReadActivity.kt
@@ -3,15 +3,26 @@ package com.example.lexiapp.ui.games.letsread
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.lifecycle.ViewModelProvider
 import com.example.lexiapp.databinding.ActivityLetsReadBinding
 
 class LetsReadActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLetsReadBinding
+    //Should be inject
+    private lateinit var vM: TextViewModel
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityLetsReadBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
+        setVM()
+    }
+
+    private fun setVM(){
+        //Should be inject
+        val factory = TextViewModel.Factory() // Factory
+        vM= ViewModelProvider(this, factory)[TextViewModel::class.java]
     }
 }
