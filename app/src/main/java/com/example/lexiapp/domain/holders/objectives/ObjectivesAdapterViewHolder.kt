79c187003dd3c87218d5dd78d66d2e3f0b6ccb89
Lucas-Zacarias <lucas21111999@gmail.com>
2023-05-15 19:50:37 -0300

    patient objectives screen first look

---
+++ app/src/main/java/com/example/lexiapp/domain/holders/objectives/ObjectivesAdapterViewHolder.kt
@@ -0,0 +1,6 @@
+package com.example.lexiapp.domain.holders.objectives
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.lexiapp.databinding.ObjectiveItemBinding
+
+class ObjectivesAdapterViewHolder(val binding: ObjectiveItemBinding): RecyclerView.ViewHolder(binding.root)
--- app/src/main/java/com/example/lexiapp/ui/objectives/ObjectivesFragment.kt
+++ app/src/main/java/com/example/lexiapp/ui/objectives/ObjectivesFragment.kt
@@ -1,16 +1,24 @@
 package com.example.lexiapp.ui.objectives
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.lexiapp.databinding.FragmentObjectivesBinding
+import com.example.lexiapp.domain.adapters.objectives.ObjectivesAdapter
+import com.example.lexiapp.domain.model.Objective
+import com.example.lexiapp.domain.repositories.objectives.ObjectiveMocks
 
 
 class ObjectivesFragment : Fragment() {
     private var _binding: FragmentObjectivesBinding? = null
     private val binding get() = _binding!!
+    private lateinit var objectiveList: List<Objective>
+    private lateinit var rvObjective: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +26,30 @@ class ObjectivesFragment : Fragment() {
     ): View {
         _binding = FragmentObjectivesBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUpRecyclerView()
+    }
+
+    private fun getObjectives() {
+        objectiveList = ObjectiveMocks.getObjectiveMocks()
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun setUpRecyclerView() {
+        rvObjective = binding.rvObjectives
+        rvObjective.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+
+        getObjectives()
+
+        val adapter = ObjectivesAdapter()
+        rvObjective.adapter = adapter
+        adapter.updateObjectiveList(objectiveList)
+        adapter.notifyDataSetChanged()
     }
 
 }
