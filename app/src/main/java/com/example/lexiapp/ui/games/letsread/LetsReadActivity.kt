0bf93eab5e737f701e672aaa604e9f72996d0e07
Lucas Zacarias <lucas21111999@gmail.com>
2023-05-15 23:02:27 -0300

    send text data from list text to text to read

--- app/src/main/java/com/example/lexiapp/ui/games/letsread/LetsReadActivity.kt
+++ app/src/main/java/com/example/lexiapp/ui/games/letsread/LetsReadActivity.kt
@@ -2,16 +2,40 @@ package com.example.lexiapp.ui.games.letsread
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import android.view.LayoutInflater
 import com.example.lexiapp.databinding.ActivityLetsReadBinding
+import com.example.lexiapp.domain.model.TextToRead
+import com.google.gson.Gson
+import java.util.*
 
 class LetsReadActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLetsReadBinding
+    private lateinit var textToSpeech: TextToSpeech
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityLetsReadBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
+
+        val gson = Gson()
+        val textToRead = gson.fromJson(intent.getStringExtra("TextToRead"), TextToRead::class.java)
+        binding.txtNameText.text = textToRead.title
+        binding.tvText.text = textToRead.text
+
+        val language = Locale("es", "US")
+        textToSpeech = TextToSpeech(this) {
+            if (it != TextToSpeech.ERROR) {
+                textToSpeech.language = language
+            }
+        }
+
+        binding.volume.setOnClickListener {
+            if (binding.tvText.text.isNotEmpty()) {
+                textToSpeech
+                    .speak(binding.tvText.text.toString(), TextToSpeech.QUEUE_FLUSH, null)
+            }
+        }
     }
 }
