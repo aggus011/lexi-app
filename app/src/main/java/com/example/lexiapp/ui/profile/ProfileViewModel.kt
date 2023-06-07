ebb9a34132180c40b9ca77b57450a1ff2234c736
aitorLob0 <aolaechealobo@alumno.unlam.edu.ar>
2023-06-06 23:57:36 -0300

    finish

--- app/src/main/java/com/example/lexiapp/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/lexiapp/ui/profile/ProfileViewModel.kt
@@ -1,6 +1,9 @@
 package com.example.lexiapp.ui.profile
 
 import android.graphics.Bitmap
+import android.os.CountDownTimer
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.lexiapp.domain.useCases.CodeQRUseCases
 import com.example.lexiapp.domain.useCases.LoginUseCases
@@ -13,9 +16,22 @@ class ProfileViewModel @Inject constructor(
     private val qrUseCases: CodeQRUseCases
 ): ViewModel() {
 
+
+    private val _timeLeftInMillis = MutableLiveData<Long>()
+    val timeLeftInMillis: LiveData<Long> = _timeLeftInMillis
+
+    private var countdownTimer: CountDownTimer? = null
+    private val initialTimeInMillis: Long = 60000 // Tiempo inicial en milisegundos (60 segundos)
+
+    private val _isTimerRunning = MutableLiveData<Boolean>()
+    val isTimerRunning: LiveData<Boolean> = _isTimerRunning
+
     private var myQR:Bitmap?=null
 
     init{
+        _timeLeftInMillis.value = initialTimeInMillis
+        _isTimerRunning.value = true
+        //Set in ProfileFragment
         setQR()
     }
 
@@ -27,4 +43,33 @@ class ProfileViewModel @Inject constructor(
 
     //Should be from SharedPreferences (or UseCases)
     private fun getEmail()="aaaa@gmail.com"
+
+    fun startTimer() {
+        if (countdownTimer != null) {
+            countdownTimer?.cancel()
+            countdownTimer = null
+        }
+
+        countdownTimer = object : CountDownTimer(_timeLeftInMillis.value ?: initialTimeInMillis, 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                _timeLeftInMillis.value = millisUntilFinished
+            }
+
+            override fun onFinish() {
+                _isTimerRunning.value = false
+                // Acción a realizar cuando el contador llegue a cero
+            }
+        }
+
+        _isTimerRunning.value = true
+        countdownTimer?.start()
+    }
+
+    fun cleared()=onCleared()
+
+    override fun onCleared() {
+        super.onCleared()
+        countdownTimer?.cancel()
+    }
 }
--- app/src/main/java/com/example/lexiapp/ui/profile/vinculate/LinkPatientActivity.kt
+++ app/src/main/java/com/example/lexiapp/ui/profile/vinculate/LinkPatientActivity.kt
@@ -14,42 +14,38 @@ class LinkPatientActivity : AppCompatActivity() {
     lateinit var binding: ActivityLinkPatientBinding
     private val vM: ProfileViewModel by viewModels()
 
-    private lateinit var countdownTimer: CountDownTimer
-    private var timeLeftInMillis: Long = 60000
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLinkPatientBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
         setQr()
+        setObservers()
+        vM.startTimer()
     }
 
     private fun setQr(){
         binding.ivQr.setImageBitmap(vM.getQR())
     }
 
-    private fun startTimer() {
-        countdownTimer = object : CountDownTimer(timeLeftInMillis, 1000) {
-            override fun onTick(millisUntilFinished: Long) {
-                timeLeftInMillis = millisUntilFinished
-                val seconds = millisUntilFinished / 1000
-                binding.txtTimerCount.text = seconds.toString()
-            }
+    private fun setObservers(){
+        vM.timeLeftInMillis.observe(this) { timeLeftInMillis ->
+            binding.txtTimerCount.text = "${(timeLeftInMillis / 1000).toString()}s"
+        }
 
-            override fun onFinish() {
+        vM.isTimerRunning.observe(this) { isRunning ->
+            if (!isRunning) {
                 finish()
             }
         }
-        countdownTimer.start()
     }
 
-    override fun onResume() {
-        super.onResume()
-        startTimer()
+    override fun onStart() {
+        super.onStart()
+        vM.startTimer()
     }
 
     override fun onPause() {
         super.onPause()
-        countdownTimer.cancel()
+        vM.cleared()
     }
 }
