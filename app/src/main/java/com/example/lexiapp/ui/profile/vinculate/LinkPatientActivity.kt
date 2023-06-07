e6d50bddff27530051bd3b0a7c29cde6c76d095c
aitorLob0 <aolaechealobo@alumno.unlam.edu.ar>
2023-06-06 23:08:02 -0300

    ..

--- app/src/main/java/com/example/lexiapp/ui/profile/vinculate/LinkPatientActivity.kt
+++ app/src/main/java/com/example/lexiapp/ui/profile/vinculate/LinkPatientActivity.kt
@@ -2,6 +2,7 @@ package com.example.lexiapp.ui.profile.vinculate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.LayoutInflater
 import androidx.activity.viewModels
 import com.example.lexiapp.databinding.ActivityLinkPatientBinding
@@ -13,20 +14,42 @@ class LinkPatientActivity : AppCompatActivity() {
     lateinit var binding: ActivityLinkPatientBinding
     private val vM: ProfileViewModel by viewModels()
 
+    private lateinit var countdownTimer: CountDownTimer
+    private var timeLeftInMillis: Long = 60000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLinkPatientBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
-        setListeners()
         setQr()
-    }
-
-    private fun setListeners() {
-
     }
 
     private fun setQr(){
         binding.ivQr.setImageBitmap(vM.getQR())
     }
 
+    private fun startTimer() {
+        countdownTimer = object : CountDownTimer(timeLeftInMillis, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                timeLeftInMillis = millisUntilFinished
+                val seconds = millisUntilFinished / 1000
+                binding.txtTimerCount.text = seconds.toString()
+            }
+
+            override fun onFinish() {
+                finish()
+            }
+        }
+        countdownTimer.start()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startTimer()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        countdownTimer.cancel()
+    }
 }
