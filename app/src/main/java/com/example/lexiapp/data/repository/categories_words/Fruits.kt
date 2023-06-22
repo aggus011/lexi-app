efe16986ec3e8d4e15f405fd2998784ccb5019d1
Lucas-Zacarias <lucas21111999@gmail.com>
2023-06-22 19:35:05 -0300

    words for stimulus by category added

---
+++ app/src/main/java/com/example/lexiapp/data/repository/categories_words/Fruits.kt
@@ -0,0 +1,26 @@
+package com.example.lexiapp.data.repository.categories_words
+
+object Fruits {
+
+    fun stimulus() = listOf(
+        "Manzana",
+        "Plátano",
+        "Naranja",
+        "Fresa",
+        "Uva",
+        "Piña",
+        "Mango",
+        "Melón",
+        "Pera",
+        "Kiwi",
+        "Cereza",
+        "Durazno",
+        "Limón",
+        "Moras",
+        "Coco",
+        "Mora",
+        "Guava",
+        "Cítrico"
+    )
+
+}
