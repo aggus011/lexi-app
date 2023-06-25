60d3858e699a016f55521f963e7091cad32f7a12
Lucas-Zacarias <lucas21111999@gmail.com>
2023-06-25 19:42:58 -0300

    change design in results of game correct word

--- app/src/main/java/com/example/lexiapp/ui/games/correctword/result/PositiveResultCorrectWordActivity.kt
+++ app/src/main/java/com/example/lexiapp/ui/games/correctword/result/PositiveResultCorrectWordActivity.kt
@@ -1,23 +1,45 @@
-    package com.example.lexiapp.ui.games.correctword.result
+package com.example.lexiapp.ui.games.correctword.result
 
-    import androidx.appcompat.app.AppCompatActivity
-    import android.os.Bundle
-    import android.view.LayoutInflater
-    import androidx.activity.viewModels
-    import com.example.lexiapp.databinding.ActivityCorrectWordBinding
-    import com.example.lexiapp.databinding.ActivityPositiveResultCorrectWordBinding
-    import com.example.lexiapp.ui.games.correctword.CorrectWordViewModel
-    import dagger.hilt.android.AndroidEntryPoint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.activity.viewModels
+import com.example.lexiapp.databinding.ActivityCorrectWordBinding
+import com.example.lexiapp.databinding.ActivityPositiveResultCorrectWordBinding
+import com.example.lexiapp.ui.games.correctword.CorrectWordActivity
+import com.example.lexiapp.ui.games.correctword.CorrectWordViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-    @AndroidEntryPoint
-    class PositiveResultCorrectWordActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class PositiveResultCorrectWordActivity : AppCompatActivity() {
 
-        lateinit var binding: ActivityPositiveResultCorrectWordBinding
-        private val vM: CorrectWordViewModel by viewModels()
+    lateinit var binding: ActivityPositiveResultCorrectWordBinding
+    private val vM: CorrectWordViewModel by viewModels()
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            binding = ActivityPositiveResultCorrectWordBinding.inflate(LayoutInflater.from(this))
-            setContentView(binding.root)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPositiveResultCorrectWordBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+
+        setListeners()
+    }
+
+    private fun setListeners() {
+        goToNextWord()
+        goToHome()
+    }
+
+    private fun goToNextWord() {
+        binding.btnNextWord.setOnClickListener {
+            startActivity(Intent(this, CorrectWordActivity::class.java))
+            finish()
         }
     }
+
+    private fun goToHome() {
+        binding.btnGoInit.setOnClickListener {
+            finish()
+        }
+    }
+}
