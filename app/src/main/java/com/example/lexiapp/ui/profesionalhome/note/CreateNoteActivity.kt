094e2d68d90407b87842bfccb999cd4b1d78c9d3
Lucas-Zacarias <lucas21111999@gmail.com>
2023-07-03 13:59:57 -0300

    visual changes in link screen results, chars of note counter added

--- app/src/main/java/com/example/lexiapp/ui/profesionalhome/note/CreateNoteActivity.kt
+++ app/src/main/java/com/example/lexiapp/ui/profesionalhome/note/CreateNoteActivity.kt
@@ -2,9 +2,14 @@ package com.example.lexiapp.ui.profesionalhome.note
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
+import com.example.lexiapp.R
 import com.example.lexiapp.databinding.ActivityCreateNoteBinding
 import com.example.lexiapp.domain.model.FirebaseResult
 import com.example.lexiapp.domain.model.User
@@ -15,6 +20,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class CreateNoteActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCreateNoteBinding
     private val vM: NoteViewModel by viewModels()
+    private lateinit var noteCharsCounter: TextView
 
     private lateinit var email: String
 
@@ -23,8 +29,31 @@ class CreateNoteActivity : AppCompatActivity() {
         binding = ActivityCreateNoteBinding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
         setPatientData(Gson().fromJson(intent.getStringExtra("patient"), User::class.java))
+        setNoteCharsCounter()
         setListener()
         setObserver()
+    }
+
+    private fun setNoteCharsCounter() {
+        noteCharsCounter = binding.tvNoteCharsCounter
+
+        binding.txtNote.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                val remainingCharacters = NOTE_MAX_LENGTH - s?.length!!
+                val color = when {
+                    remainingCharacters >= 100 -> R.color.green
+                    remainingCharacters >= 50 -> R.color.pear_green
+                    else -> R.color.red
+                }
+
+                noteCharsCounter.text = remainingCharacters.toString().plus("/200")
+                noteCharsCounter.setTextColor(ContextCompat.getColor(this@CreateNoteActivity, color))
+            }
+        })
     }
 
     private fun setPatientData(patient: User) {
@@ -64,6 +93,10 @@ class CreateNoteActivity : AppCompatActivity() {
 
     private fun toast(txtToast: String) {
         Toast.makeText(this, txtToast, Toast.LENGTH_SHORT).show()
+    }
+
+    companion object{
+        private const val NOTE_MAX_LENGTH = 200
     }
 
 }
