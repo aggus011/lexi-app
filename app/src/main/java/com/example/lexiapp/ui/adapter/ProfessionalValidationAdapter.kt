b0d1044eed1d77eb6158f5214c7889f43c42802c
Brian <brtabarez@alumno.unlam.edu.ar>
2023-07-05 20:02:36 -0300

    recycler admin

--- app/src/main/java/com/example/lexiapp/ui/adapter/ProfessionalValidationAdapter.kt
+++ app/src/main/java/com/example/lexiapp/ui/adapter/ProfessionalValidationAdapter.kt
@@ -9,14 +9,18 @@ import com.example.lexiapp.domain.model.ProfessionalValidation
 class ProfessionalValidationAdapter(
     private val professionals: List<ProfessionalValidation>,
     private val setApproval: (String, Boolean) -> Unit
-): RecyclerView.Adapter<ProfessionalValidationAdapter.ProfessionalValidationViewHolder>() {
+) : RecyclerView.Adapter<ProfessionalValidationAdapter.ProfessionalValidationViewHolder>() {
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): ProfessionalValidationAdapter.ProfessionalValidationViewHolder {
         return ProfessionalValidationViewHolder(
-            ItemProfessionalVerificationBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            ItemProfessionalVerificationBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
         )
     }
 
@@ -31,19 +35,32 @@ class ProfessionalValidationAdapter(
 
     override fun getItemCount() = professionals.size
 
-    inner class ProfessionalValidationViewHolder(val binding: ItemProfessionalVerificationBinding): RecyclerView.ViewHolder(binding.root) {
+    inner class ProfessionalValidationViewHolder(val binding: ItemProfessionalVerificationBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(professional: ProfessionalValidation){
-            binding.tvProfessionalName.text = professional.name
-            binding.tvProfessionalEmail.text = professional.email
-            binding.tvMedicalRegistration.text = professional.medicalRegistration
+        fun bind(professional: ProfessionalValidation) {
+            binding.txtName.text = professional.name
+            binding.tvUserInitials.text = userInitials(professional.name)
+            binding.txtEmail.text = professional.email
+            binding.txtMat.text = professional.medicalRegistration
             binding.switchValidate.isChecked = professional.validated
         }
 
         fun setSwitchListener(professional: ProfessionalValidation) {
-            binding.switchValidate.setOnCheckedChangeListener{ _, isChecked ->
+            binding.switchValidate.setOnCheckedChangeListener { _, isChecked ->
                 setApproval(professional.email, isChecked)
             }
+        }
+
+        private fun userInitials(name: String): String {
+            val words = name.split(" ")
+            val initials = StringBuilder()
+            for (word in words) {
+                val initial = word.firstOrNull()
+                if (initial != null)
+                    initials.append(initial)
+            }
+            return initials.toString()
         }
     }
 }
