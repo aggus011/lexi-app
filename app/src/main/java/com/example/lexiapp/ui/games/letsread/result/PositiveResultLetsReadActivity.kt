7f707bc6030d803c1d98dd0435577136de72a5b7
Lucas Zacarias <lucas21111999@gmail.com>
2023-07-07 23:48:36 -0300

    refactor

--- app/src/main/java/com/example/lexiapp/ui/games/letsread/result/PositiveResultLetsReadActivity.kt
+++ app/src/main/java/com/example/lexiapp/ui/games/letsread/result/PositiveResultLetsReadActivity.kt
@@ -1,23 +1,18 @@
 package com.example.lexiapp.ui.games.letsread.result
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.widget.Button
-import androidx.activity.viewModels
-import com.example.lexiapp.databinding.ActivityNegativeResultCorrectWordBinding
+import androidx.appcompat.app.AppCompatActivity
 import com.example.lexiapp.databinding.ActivityPositiveResultLetsReadBinding
-import com.example.lexiapp.ui.games.correctword.CorrectWordViewModel
 import com.example.lexiapp.ui.games.letsread.ListTextActivity
-import com.example.lexiapp.ui.games.letsread.TextViewModel
 import com.example.lexiapp.ui.patienthome.HomePatientActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class PositiveResultLetsReadActivity : AppCompatActivity() {
     lateinit var binding: ActivityPositiveResultLetsReadBinding
-    private val vM: TextViewModel by viewModels()
 
     private lateinit var nextTextBtn: Button
     private lateinit var homeBtn: Button
