dd824f1a40d427a948de41b9a622ada55bb0cde0
Leandro <leandrochazarreta96@gmail.com>
2023-07-13 14:25:31 -0300

    test/ProfileUseCasesTest

--- app/src/test/java/com/example/lexiapp/domain/useCases/ProfileUseCasesTest.kt
+++ app/src/test/java/com/example/lexiapp/domain/useCases/ProfileUseCasesTest.kt
@@ -2,13 +2,17 @@ package com.example.lexiapp.domain.useCases
 
 import android.content.SharedPreferences
 import com.example.lexiapp.R
+import com.example.lexiapp.domain.model.User
+import com.example.lexiapp.domain.model.UserLogin
 import com.example.lexiapp.domain.service.FireStoreService
-import io.mockk.MockKAnnotations
-import io.mockk.every
+import io.mockk.*
 import io.mockk.impl.annotations.RelaxedMockK
 import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
 import org.junit.Before
 import org.junit.Test
+import java.util.*
 
 class ProfileUseCasesTest{
     @RelaxedMockK
@@ -42,6 +46,79 @@ class ProfileUseCasesTest{
         val inits = profileUseCases.userInitials()
         //Then
         assert(inits == "NL")
+    }
+
+
+    @Test
+    fun `when getting the professional verification state, it should return the saved verification state from SharedPreferences`() {
+        // Given
+        val verificationState = 2
+
+        every { prefs.getInt("professional_account_state", 0) } returns verificationState
+
+        // When
+        val result = profileUseCases.getProfessionalVerificationState()
+
+        // Then
+        assertEquals(result, verificationState)
+    }
+
+
+    @Test
+    fun `when getting the user type, it should return the saved user type from SharedPreferences`() {
+        // Given
+        val userType = "profesional"
+
+        every { prefs.getString("user_type", null) } returns userType
+
+        // When
+        val result = profileUseCases.getUserType()
+
+        // Then
+        assertEquals(result, userType)
+    }
+
+    @Test
+    fun `when editing a profile, it should save the modified user in Firestore`() = runBlocking {
+        // Given
+        val userName = completedName
+        val professional = "12345"
+        val birthDate = null
+
+        val modifiedUser = User(email = "asd@asd.com", userName = completedName, birthDate = null, profesional = professional)
+
+        coEvery { prefs.getString("user_name", null) } returns userName
+
+        // When
+        val result = profileUseCases.editProfile(completedName, professional, birthDate)
+
+        // Then
+        assertEquals(modifiedUser.userName, result.userName)
+        coVerify { fireStoreService.saveAccount(result) }
+    }
+
+    @Test
+    fun `when checking if there is an account, it should return false if email is null`() {
+        // Given
+        every { prefs.getString("email", null) } returns null
+
+        // When
+        val result = profileUseCases.haveAccount()
+
+        // Then
+        assert(!result)
+    }
+
+    @Test
+    fun `when checking if there is an account, it should return true if email is not null`() {
+        // Given
+        every { prefs.getString("email", null) } returns "test@example.com"
+
+        // When
+        val result = profileUseCases.haveAccount()
+
+        // Then
+        assert(result)
     }
 
 
